479bb601627404eebc555e98aa6a68505c14dcc8
thtl1999 <thtl2004@hanmail.net>
2020-07-17 15:53:15 +0900

    feature: text color

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let WORD_DB
+let colors = ['color: #730240', 'color: #304173', 'color: #F2CB05', 'color: #594B04', 'color: #0D0D0D']
+let color_index = 0
 
 fetch('/cardnames.json')
     .then(response => {
@@ -8,8 +10,13 @@ fetch('/cardnames.json')
         WORD_DB = [...new Set(word_json)]
     })
 
+function get_color(){
+    color_index = (color_index + 1) % colors.length
+    return colors[color_index]
+}
+
 function add_card_result(){
-    
+
 }
 
 function make_collage(){
