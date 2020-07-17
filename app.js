32211cd36465b172c251fa9a64915b13e43d5415
thtl1999 <thtl2004@hanmail.net>
2020-07-18 03:10:46 +0900

    feature: find optimal substrings

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 let WORD_DB
 let colors = ['color: #730240', 'color: #304173', 'color: #F2CB05', 'color: #594B04', 'color: #0D0D0D']
 let color_index = 0
+const removable_chars = [' ', '·', '－', '.', '「', '」']
+let unused_substrings = []
+
+let test = 'CNo.92 위해허룡 Heart－eartH Chaos Dragon'
 
 fetch('/cardnames.json')
     .then(response => {
@@ -19,7 +23,118 @@ function add_card_result(){
 
 }
 
+class Substr{
+    constructor(card_name, card_start, card_end, user_start, user_end){
+        this.card_name = card_name
+        this.card_start = card_start
+        this.card_end = card_end
+        this.user_start = user_start
+        this.user_end = user_end
+    }
+
+    is_contain(user_start, user_end){
+        if (user_start >= this.user_start && user_end <= this.user_end)
+            return true
+        else
+            return false
+    }
+
+    has_index(user_start){
+        if (user_start >= this.user_start && user_start <= this.user_end)
+            return true
+        else
+            return false
+    }
+}
+
 function make_collage(){
+    
     let user_string = document.getElementById('user-string').value
     
+}
+
+function get_all_substrings(user_string){
+    unused_substrings = []
+    let substrings = []
+
+    WORD_DB.forEach(card_name => {
+        // Find substrings in a word
+        let card_substring_array = get_substrings(user_string, card_name)
+        card_substring_array.forEach(substr => {
+            // Try add to substring in array
+            add_substring(substrings, substr)
+        })
+    })
+
+    console.log(substrings)
+    return substrings
+}
+
+function add_substring(substr_array, substr){
+    for (let i = substr_array.length - 1; i >= 0; i--) {
+        let s = substr_array[i]
+
+        // If longer substring exist, exit
+        if (s.is_contain(substr.user_start, substr.user_end)){
+            unused_substrings.push(substr)
+            return
+        }
+
+        // If shorter substring exist, delete shorter
+        if (substr.is_contain(s.user_start, s.user_end)){
+            substr_array.splice(i, 1)
+        }
+            
+
+    }
+
+    // Append substring array
+    substr_array.push(substr)
+}
+
+function get_substrings(user_string, card_name){
+    let substr_array = []
+
+    for(let card_index = 0; card_index < card_name.length; card_index++){
+        for (let user_index = 0; user_index < user_string.length; user_index++){
+            // Do not start searching on removable char
+            if (is_removable(card_name[card_index]) || is_removable(user_string[user_index]))
+                continue
+
+            let card_strlen = 0
+            let user_strlen = 0
+
+            while(card_index + card_strlen < card_name.length && user_index + user_strlen < user_string.length){
+                let card_char = card_name[card_index + card_strlen]
+                let user_char = user_string[user_index + user_strlen]
+                
+                // Jump removable characters
+                if (is_removable(card_char)){
+                    card_strlen++
+                    continue
+                }
+                if (is_removable(user_char)){
+                    user_strlen++
+                    continue
+                }
+
+                if (card_char.toLowerCase() === user_char.toLowerCase()){
+                    // If found substring
+                    let substr = new Substr(card_name, card_index, card_index + card_strlen, user_index, user_index + user_strlen)
+                    add_substring(substr_array, substr)
+                }
+                else
+                    break
+
+                card_strlen++
+                user_strlen++
+            }
+        }
+    }
+
+    return substr_array
+}
+
+function is_removable(char){
+    return removable_chars.includes(char)
 }
